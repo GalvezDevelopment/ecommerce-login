02f49cf09b775720de144eaff56b1158aa9a6587
Christian Aurelio Galvez Javalera <christiangalvez@Christians-MacBook-Air.local>
2024-03-04 12:44:22 -0700

    Lazy loading for the modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+      {
+        path: 'cart',
+        loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
+      },
+      {
+        path: 'checkout',
+        loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
+      }
+    ]
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.component').then(m => m.LoginComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
