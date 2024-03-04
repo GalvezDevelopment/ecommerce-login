bf04f03ec250186887308041d1db6595404ad56d
Christian Aurelio Galvez Javalera <christiangalvez@Christians-MacBook-Air.local>
2024-03-04 12:56:14 -0700

    Dashboard's structure and routing defined.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoreModule } from '../core/core.module';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ItemDetailComponent } from './pages/item-detail/item-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        component: CatalogComponent
+      },
+      {
+        path: 'detail/:id',
+        component: ItemDetailComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CatalogComponent,
+    ItemDetailComponent,
+    DashboardComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    CoreModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class DashboardModule { }
